47bf5ebeb2a91f1d4d72a89e4c99f8c162336d1c
ShayanEm <150065407+ShayanEm@users.noreply.github.com>
2023-11-26 14:09:58 -0500

    v2

--- Face Detection (Cross Compilation)/Common/imageCommun.h
+++ Face Detection (Cross Compilation)/Common/imageCommun.h
@@ -6,11 +6,9 @@
 #define QUIT 0x00000000          // clinet message to quit
 #define RES1_OK 0x00000003
 #define RES2_OK 0x00000007
-
-#define READY 0x00000008		// light on
-#define IDOWN 0x00000010		// light off
-#define PUSHB 0x00000018		// light on and btn on
-//#define PORT 4099
+#define READY 0x00000008                // light on
+#define IDOWN 0x00000010                // light off
+#define PUSHB 0x00000018                // light on and btn on
 
 //Client
 #define PORT 4099          // The port number for communication
@@ -18,14 +16,7 @@
 #define ESC 1048603        // ESC key value
 #define K1 1048625         // Number 1 key value
 #define K2 1048626         // Number 2 key value
-
-//#define OK 0x00000001            // client message to continue
-//#define QUIT 0x00000000          // clinet message to quit
-#define RESO1 0x00000002 
-#define RESO2 0x00000006 
-
-//#define READY 0x00000008		// light on
-//#define IDOWN 0x00000010		// light off
-//#define PUSHB 0x00000018		// light on and btn on
+#define RESO1 0x00000002
+#define RESO2 0x00000006
 
 #endif  // IMAGE_COMMUN_H
