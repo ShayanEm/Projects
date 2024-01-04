7fee72b19207d6f5e441aac1b7c35ee75af05528
ShayanEm <150065407+ShayanEm@users.noreply.github.com>
2024-01-04 15:44:26 -0500

    Version 2.0

--- Website/script.js
+++ Website/script.js
@@ -1,4 +1,19 @@
-// JavaScript function to display an alert when the logo is clicked
+/**
+ * @file script.js
+ * @brief JavaScript file for Shayan's website.
+ * 
+ * Contains functions for handling user interactions and displaying alerts.
+ *
+ * @author Shayan Eram
+ */
+
+/**
+ * @function showClickAlert
+ * @brief Displays an alert when the logo is clicked.
+ * @details This function is triggered by the onclick event of the logo.
+ * 
+ * @memberof global
+ */
 function showClickAlert() {
     alert("Shayan Eram"); 
 }
